89aea55bb170c766922b6c358e9f0652a349a93f
Dima <dimashikalov86@mail.ru>
2023-09-20 15:46:36 +0400

    added adaptive for pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import styles from "./header.module.css";
 import cn from "classnames";
 import { authExit } from "../../store/auth/authSlice";
 import { Button } from "../Button/Button";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 export const Header = ({ children, hasBackButton = false }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const { width } = useWindowDimensions();
+
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
@@ -18,7 +22,7 @@ export const Header = ({ children, hasBackButton = false }) => {
             size="small"
             onClick={() => navigate(-1)}
           >
-            Назад
+            {width > 640 ? "Назад" : ""}
           </Button>
         )}
 
@@ -28,7 +32,7 @@ export const Header = ({ children, hasBackButton = false }) => {
           size="small"
           onClick={() => dispatch(authExit())}
         >
-          Выход
+          {width > 640 ? "Выход" : ""}
         </Button>
       </div>
     </header>
